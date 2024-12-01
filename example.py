a207be45c5728827e3079becc1d4116e677f2245
Luis Du <luisdu.s23@gmail.com>
2024-12-01 18:42:50 +0000

    open map implemented

--- example.py
+++ example.py
@@ -7,11 +7,18 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 
 numAgents = 1
 
-env = gym.make('CommonsGame:CommonsGame-v0', map_config=SmallMap, visual_radius=4)
-env.reset()
-for t in range(50):
-    nActions = np.random.randint(low=0, high=env.action_space.n, size=(numAgents,)).tolist()
-    nObservations, nRewards, nDone, nInfo = env.step(nActions)
+env = gym.make('CommonsGame:CommonsGame-v0', map_config=OpenMap, visual_radius=4)
+env.reset()\
+    
+    
+inp = ''
+# for t in range(50):
+#     nActions = np.random.randint(low=0, high=env.action_space.n, size=(numAgents,)).tolist()
+#     nObservations, nRewards, nDone, nInfo = env.step(nActions)
+#     env.render()
+#     if t == 9:
+#         print(nObservations[0])
+
+while inp != 'q':
     env.render()
-    if t == 9:
-        print(nObservations[0])
+    inp = input('Press q to exit: ')
