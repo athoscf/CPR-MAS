bac0e2d0d08d963ddaa3577ad0d8c46f41878bc7
Luis Du <luisdu.s23@gmail.com>
2024-12-04 18:25:20 +0000

    setup updated

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym-commons-game',
       version='0.0.1',
+      packages=find_packages(),
       install_requires=['gym', 'pycolab', 'matplotlib', 'numpy', 'scipy', 'torch', 'torchvision', 'torchaudio', 'pandas']
 )
