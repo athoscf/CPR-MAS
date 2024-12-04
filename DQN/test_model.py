93693769aa1bedef6bcb4190807253a60e412cbd
Athos Coghi Freitas <93884747+athoscf@users.noreply.github.com>
2024-12-04 19:55:49 +0000

    Save csv (#11)
    * Updated maps
    
    * save to csv implemented

--- DQN/test_model.py
+++ DQN/test_model.py
@@ -13,7 +13,7 @@ matplotlib.use('Agg')
 
 class TestModel():
     
-    def __init__(self, num_episodes=5000, map=SmallMap, visual_radius=5, warmup_steps=1000, action_policy=ActionPolicies.TAG_ONLY): 
+    def __init__(self, num_episodes=5000, map=SmallMap, visual_radius=5, warmup_steps=1000, action_policy=ActionPolicies.TAG_ONLY, csv_filename="result.csv"): 
         self.num_episodes = num_episodes
         self.num_agents = map.num_agents
         self.visual_radius = visual_radius
@@ -24,6 +24,8 @@ class TestModel():
         self.env = gym.make('CommonsGame:CommonsGame-v0', map_config=map, visual_radius=visual_radius)
 
         self.agents = self.create_agents(action_policy)
+        
+        self.csv_filename = csv_filename
 
     def execute(self):
         print("warming up replay buffer...")
@@ -160,4 +162,7 @@ class TestModel():
             thread.start()
 
         print('Episode {} Score: {:.2f} Average Score: {:.2f} Epsilon {:.2f}'.format(episode, scores[-1], avg_score, self.agents[0].epsilon))
+        
+        if episode % 5 == 0:
+            save_as_csv(metrics_values, episode, self.csv_filename)
         
