44b78b600c141f16283ce3b0a5eebbcd2c57f5ce
Luis Du <luisdu.s23@gmail.com>
2024-12-06 19:16:55 +0000

    changing 'gpu' to 'cuda'

--- DQN/deep_q_network.py
+++ DQN/deep_q_network.py
@@ -6,7 +6,7 @@ class DeepQNetwork(nn.Module):
     def __init__(self, input_dims, n_actions):
       super(DeepQNetwork, self).__init__()
 
-      self.device = torch.device('gpu' if torch.cuda.is_available() else 'cpu')
+      self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
       
       self.input_dims = input_dims
       self.n_actions = n_actions
