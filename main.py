ba444c51e9ceb2bdef034728204f44a30890a7c5
José Santos <sjose7780@gmail.com>
2024-12-15 19:27:54 +0000

    Project done!

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from CommonsGame import *
 from DQN import *
 
 def main():    
-    game = TestModel(map=SmallMapWalls, action_policy=ActionPolicies.TAG_AND_GIFT, num_episodes=2001, visual_radius=5, warmup_steps=5000)
-    game.execute()
-
-    game = TestModel(map=SmallMapWalls, action_policy=ActionPolicies.MIXED, num_episodes=2001, visual_radius=5, warmup_steps=5000)
+    game = TestModel(map=SmallMap, action_policy=ActionPolicies.TAG_AND_GIFT, num_episodes=1501, visual_radius=5, warmup_steps=5000)
     game.execute()
 
 if __name__ == "__main__":
